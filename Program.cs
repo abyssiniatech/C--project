44f93fbbe2a52da0f34e161d1f0d859a07394bdd
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-19 19:33:17 +0300

    add c# course day1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const String name="surafel Mengist";
+        const int age=28;
+        const String Address ="Addis Ababa";
+        const String greet ="welcome to me";
+      
+        Console.WriteLine("Hello"+ greet+ "my name is :" + name + "and" + "I'm " + age + "years old" + "i live in " + address);
+    }
+}
