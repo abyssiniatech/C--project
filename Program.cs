7b2d7787cfe66c0a42a2126a1094450fd938c667
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-19 20:11:54 +0300

    add Hello world

--- Program.cs
+++ Program.cs
@@ -4,11 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        const String name="surafel Mengist";
-        const int age=28;
-        const String Address ="Addis Ababa";
-        const String greet ="welcome to me";
+         String name="surafel Mengist";
+         int age=28;
+         String Address ="Addis Ababa";
+         String greet ="welcome to me";
       
         Console.WriteLine("Hello"+ greet+ "my name is :" + name + "and" + "I'm " + age + "years old" + "i live in " + address);
+        // input from the keyboard and finally display it 
+         Console.Write("Enter your Email");
+         String email=Console.ReadLine();
+         Console.WriteLine("my email is :" + email);
+       Console.ReadLine();
     }
 }
