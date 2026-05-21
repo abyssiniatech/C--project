c566801eb5ca7afaa71a35338dd73ee9263bbcd2
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-21 21:37:04 +0300

    add list in C#:

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,6 +1,6 @@
 ﻿List<Student> students = [
     new Student { Id = "1", Name = "Alice", Age = 20, Gpa = 3.5m },
-    new Student { Id = "2", Name = "Bob", Age = 22, Gpa = 3.8m },
+    new Student { Id = "2", Name = "Bob", Age = 22, Gpa = 3.9m },
     new Student { Id = "3", Name = "Charlie", Age = 19, Gpa = 3.2m }
 
 ];
@@ -14,3 +14,29 @@ else
     Console.WriteLine("No students found.");
 }
 
+// Output: Top student: Bob with GPA: 3.9
+// select top 1 * from students order by gpa desc
+// select * from students order by gpa desc limit 1
+// select * from students order by gpa desc fetch first 1 rows only
+var topStudent2 = students.OrderByDescending(s => s.Gpa).Take(1).FirstOrDefault();
+if (topStudent2 != null)
+{
+    Console.WriteLine($"Top student: {topStudent2.Name} with GPA: {topStudent2.Gpa}");
+}
+else
+{
+    Console.WriteLine("No students found.");
+}
+
+
+
+// simple dictionary
+var studentDict = students.ToDictionary(s => s.Id);
+if (studentDict.TryGetValue("2", out var student))
+{
+    Console.WriteLine($"Found student: {student.Name} with GPA: {student.Gpa}");
+}
+else
+{
+    Console.WriteLine("Student not found.");
+}
--- Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
+++ Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db59792fbbc07d5b7b5214b30050a0b8703106fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d23b960ee97f9808015a397b156bcfdad60953bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
