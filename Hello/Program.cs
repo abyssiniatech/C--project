1d393a8e4b1e1e473337f65b70789211f88c9dfc
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-23 13:39:18 +0300

    add for loop in C#:

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,65 +1,13 @@
 ﻿using System;
-
-namespace MyFirstProgram
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        for(int i=0; i <= 10; i++)
         {
-            Random random = new Random();
-            bool playAgain = true;
-            int min = 1;
-            int max = 100;
-            int guess;
-            int number;
-            int guesses;
-            String response;
-
-            while (playAgain)
-            {
-                guess = 0;
-                guesses = 0;
-                response = "";
-                number = random.Next(min, max + 1);
-
-                while (guess != number)
-                {
-                    Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
-                    guess = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Guess: " + guess);
-
-                    if (guess > number)
-                    {
-                        Console.WriteLine(guess + " is to high!");
-                    }
-                    else if (guess < number)
-                    {
-                        Console.WriteLine(guess + " is to low!");
-                    }
-                    guesses++;
-                }
-                Console.WriteLine("Number: " + number);
-                Console.WriteLine("YOU WIN!");
-                Console.WriteLine("Guesses: " + guesses);
-
-                Console.WriteLine("Would you like to play again (Y/N): ");
-                response = Console.ReadLine() ?? "";
-                response = response.ToUpper();
-
-                if (response == "Y")
-                {
-                    playAgain = true;
-                }
-                else
-                {
-                    playAgain = false;
-                }
-            }
-
-            Console.WriteLine("Thanks for playing! ... I guess");
-
-            Console.ReadKey();
+            Console.WriteLine($"The value of i is : {i}");
         }
+      
+      
     }
 }
-
--- Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
+++ Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d931decb272d79374ec5c904b8844a0b2c7c3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ebf29e71e6e87ffb00b637674ea59833ac16a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
