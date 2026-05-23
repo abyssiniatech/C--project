814d637e28e7e10805338067e8082ba9ef237fa5
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-23 14:32:16 +0300

    add array:

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,43 +1,10 @@
 ﻿using System;
-
+// array in c#
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.Write("Enter first number: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Enter operator (+, -, *, /): ");
-        char op = Console.ReadLine()[0];
-
-        Console.Write("Enter second number: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
-
-        double result = 0;
-
-        switch(op)
-        {
-            case '+':
-                result = num1 + num2;
-                break;
-
-            case '-':
-                result = num1 - num2;
-                break;
-
-            case '*':
-                result = num1 * num2;
-                break;
-
-            case '/':
-                result = num1 / num2;
-                break;
-
-            default:
-                Console.WriteLine("Invalid operator");
-                return;
-        }
-
-        Console.WriteLine("Result = " + result);
+        string[] car ={"chevrolte","BMW","Toyota"};
+        System.Console.WriteLine(car[0]);
     }
 }
--- Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
+++ Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d359a5e361c386179426d23e724c768f1a4790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f825da667382ff84472798666faaf4afa63fcba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
