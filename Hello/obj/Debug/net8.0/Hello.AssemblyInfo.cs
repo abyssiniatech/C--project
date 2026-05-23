76d931decb272d79374ec5c904b8844a0b2c7c3f
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-23 13:10:21 +0300

    switch

--- Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
+++ Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8197eb3c1dfc553540fb370468ebca30c15b01e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ccf0c0e84575e093deb4c7ec07fd548b6d88f2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
