0dba6ebd5d9ab0f558329194da8c44b4b76f2181
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-25 13:03:17 +0300

    add conditional operator:

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -5,15 +5,11 @@
 using System;
 class Program {
     static void Main(string[] args){
-        try{
-
-        string name = null;
-         Console.WriteLine(name.ToUpper());
-        }
-        catch (NullReferenceException ex)
-        {
-           Console.WriteLine("A null reference exception occurred: " + ex.Message); 
-        }
+    //    conditional operator
+    int age=34;
+     string info=(age>=18) ? "You are an adult" : "You are a minor";
+     Console.WriteLine(info);
+    
 
     }
 }
