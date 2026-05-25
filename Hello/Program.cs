2d44c7c0113c4e9c0927392bca840ff23a0d7b2c
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-25 12:59:03 +0300

    add the exception handling in c#

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿
+
+// dotnet new console -n TmsCore --framework net10.0
+
+using System;
 class Program {
-    static void main(string[] args){
-        Console.WriteLine("Hello world!");
-        Console.WriteLine("This the bet coure of dthe year");
+    static void Main(string[] args){
+        try{
+
+        string name = null;
+         Console.WriteLine(name.ToUpper());
+        }
+        catch (NullReferenceException ex)
+        {
+           Console.WriteLine("A null reference exception occurred: " + ex.Message); 
+        }
 
     }
 }
--- Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
+++ Hello/obj/Debug/net8.0/Hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2360af3de61067a8a190e65568da2c11f79c9f90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465911f7349b5e9a34e972f9c467a4a849aa1cc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
